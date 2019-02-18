135f75c54a3d75ec6ab8a11ab78ee2e487c54704
Jashmae <jbaculpo@csumb.edu>
2019-02-18 00:05:53 -0800

    finished the logout video. Starting the staying logged in video

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Jashmae Baculpo on 2/17/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
